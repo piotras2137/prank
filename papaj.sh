938e365afa519ee5c3a7b085313044fd48ea729d
Piotr Martyniuk <58687899+piotras2137@users.noreply.github.com>
2022-08-29 11:42:51 +0200

    Update papaj.sh

--- papaj.sh
+++ papaj.sh
@@ -1,2 +1,3 @@
-firefox 'https://img.obrazkowo.pl/images/3076287e3854e07ed68836f0362e80d5.jpg'
-
+while [ 1 -eq 1 ] do
+  firefox 'https://img.obrazkowo.pl/images/3076287e3854e07ed68836f0362e80d5.jpg'
+done
